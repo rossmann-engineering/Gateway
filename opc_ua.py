fd761ef03dd82ad74783fd30fb9a871afa564e38
rossmann-engineering <info@rossmann-engineering.de>
2023-01-11 13:35:53 +0100

    changed config.json

--- opc_ua.py
+++ opc_ua.py
@@ -59,7 +59,7 @@ async def main(url, user, password, nodes, write=False):
                 if not write:
                     node['value'] = await child.get_value()
                 else:
-                    await child.set_value()
+                    await child.set_value(node['value'])
 
         #await child.set_value(42)
         #print(await child.get_value())
